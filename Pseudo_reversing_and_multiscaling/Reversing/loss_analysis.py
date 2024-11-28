8569d2e081e02b7d049104a00a435881a1b3e9f2
waelmattar <waelmattar.95@gmail.com>
2024-11-28 15:21:05 +0200

    loss analysis fixed

--- Pseudo_reversing_and_multiscaling/Reversing/loss_analysis.py
+++ Pseudo_reversing_and_multiscaling/Reversing/loss_analysis.py
@@ -9,15 +9,15 @@ def loss_function(alpha: list, support: list, xi: float):
     new_mask = uf.pseudo_reversing(mask=alpha.copy(), support=support.copy(), xi=xi)
     gamma = uf.gamma(mask=new_mask, support=support)
     even_mask = [new_mask[k] for k in range(len(new_mask)) if support[k] % 2 == 0]
-    delta = [0] * int(200 - len(even_alpha))
-    delta[int(100 - np.floor(len(even_alpha)/2))] = 1
-    return np.linalg.norm(np.subtract(delta, np.convolve(gamma, even_alpha, 'valid')), ord=np.infty), np.linalg.norm(np.subtract(even_mask, even_alpha), ord=np.infty)
+    delta = [0] * int(600 - len(even_alpha))
+    delta[int(300 - np.floor(len(even_alpha)/2))] = 1
+    return np.linalg.norm(np.subtract(delta, np.convolve(gamma, even_alpha, 'valid')), ord=2), np.linalg.norm(np.subtract(even_mask, even_alpha), ord=1)
 
 
 n = 2
 d = 1
 lower_xi = 1e-10
-upper_xi = 0.4
+upper_xi = 4
 xis = list(np.linspace(lower_xi, upper_xi, 200))
 alpha, support = uf.least_squares_mask_and_support(d=d, n=n)
 loss = []
@@ -34,9 +34,9 @@ plt.plot(xis, [loss[k][0] for k in range(len(loss))], alpha=1, label=r'Convoluti
 plt.plot(xis, [loss[k][1] for k in range(len(loss))], alpha=1, linestyle='dashed', label=r'Mask perturbation', linewidth=3)
 plt.xlim([lower_xi, upper_xi])
 plt.ylim(bottom=0)
-plt.xticks(lower_xi + np.linspace(lower_xi, upper_xi, 5))
+plt.xticks(lower_xi + np.linspace(lower_xi, upper_xi, 11))
 plt.tick_params(axis='both', which='major', labelsize=16)
 plt.xlabel(r'$\xi$', fontsize=15)
-plt.legend(prop={'size': 16}, loc='upper left')
+plt.legend(prop={'size': 16})
 plt.savefig('Figures/loss_analysis.pdf', format='pdf')
 plt.show()
